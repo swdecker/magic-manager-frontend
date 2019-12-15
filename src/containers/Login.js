dff5d324f222e33c2539b774c703fa0ce7d4b917
swdecker <swdecker@gmail.com>
2019-12-14 18:23:11 -0600

    basic account creation and login completed

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,33 +6,58 @@ export default class login extends Component {
         super(props)
         this.state = {
             username: "",
-            email:"",
             password: "",
             errors: ''
         }
     }
+
     handleChange = (event)=>{
         const { name, value } = event.target
         this.setState({
             [name]: value
         })
     }
+
+    handleSubmit = event =>{
+        event.preventDefault()
+        let user = { username: this.state.username, password: this.state.password}
+        this.loginUser(user)
+    }
+    loginUser = user => {
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({ user: user })
+        }
+        fetch('http://localhost:3001/api/v1/login', configObj )
+        .then(r => r.json())
+        .then(data => {
+            console.log(data) 
+            localStorage.setItem('token', data.jwt);
+            console.log(localStorage.getItem('token'))
+
+            // should also set state with user info
+        })
+    }
     render() {
         return (
             <div>
                 The login Form
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control onChange={this.handleChange} name={"username"} type="text" placeholder="Enter username" />
                         <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
+                        
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={this.handleChange} name={"password"} type="password" placeholder="Password" />
                     </Form.Group>
                     
                     <Button variant="primary" type="submit">
