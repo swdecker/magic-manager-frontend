640450bdd876d734f486b83a2f4f508209f42518
swdecker <swdecker@gmail.com>
2020-01-02 16:14:52 -0600

    add delete and styling

--- src/components/MagicNavbar.js
+++ src/components/MagicNavbar.js
@@ -1,27 +1,25 @@
 import  React from 'react'
-import {Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import {Nav, Navbar } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { logout }  from '../actions/logout'
+
+import {withRouter } from 'react-router-dom'
+import history from '../history';
 
 const MagicNavbar = (props) => {
     
     return (
         <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="/collection">Magic Manager</Navbar.Brand>
+  <Navbar.Brand >Magic Manager</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link disabled >{props.username}</Nav.Link>
-      <Nav.Link href="#home">Collection</Nav.Link>
-      <Nav.Link href="#home">Decks</Nav.Link>
+      <Nav.Link disabled >{props.currentUser.username}</Nav.Link>
+      <Nav.Link onClick={()=>history.push("/collection") }>Collection</Nav.Link>
+     <Nav.Link onClick={()=>history.push("/addCard") }>Add Card</Nav.Link>
+      
       <Nav.Link onClick={()=>props.logout()} >Logout</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      
     </Nav>
     
   </Navbar.Collapse>
@@ -34,4 +32,4 @@ const mapStateToProps = (state)=>{
 }
 
 
-export default connect(mapStateToProps, {logout})( MagicNavbar )
+export default withRouter(connect(mapStateToProps, {logout})( MagicNavbar ))
